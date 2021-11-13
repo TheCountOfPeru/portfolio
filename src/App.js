9ceb7e4bee9d5c43ea25ee478f7424350087feb7
TheCountOfPeru <webber989@gmail.com>
2021-11-13 15:55:06 -0700

    some work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header class="">
+    <div class=" fixed top-0 ">
+            <a href="_blank" class="underline font-extrabold p-2 text-5xl hover:text-gray-500">Home</a>
+          </div>
+    </header>
+
+          <div class="flex flex-col justify-center mx-32 my-52 px-96">
+          <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
+  Button
+</a>  <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
+  Button
+</a>  
+
+          </div>
+          
+    <footer class="border-t border-gray-400">
+      <div
+        class="container flex flex-wrap items-center justify-center px-4 py-8 mx-auto  lg:justify-between"
+      >
+        <div class="flex flex-wrap justify-center">
+          <ul class="flex items-center space-x-4">
+            Made by Jonathan Yee
+          </ul>
+        </div>
+        
+      </div>
+    </footer>
+    </>
   );
 }
 
